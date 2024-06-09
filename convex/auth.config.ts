a1c558b261dd0474817891c2d72a7b7ca2591fb6
Akmeena4u <ak872030@student.nitw.ac.in>
2024-06-09 16:05:14 +0530

    Initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,13 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://on-airedale-79.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
+
+
+//https://fond-sawfly-54.clerk.accounts.dev
